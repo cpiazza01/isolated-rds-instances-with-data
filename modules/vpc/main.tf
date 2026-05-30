a7eb87ad1c46172bd53ebf5b29cef43d7d4c7d1e
Cody Piazza <codypiazza@gmail.com>
2026-05-29 20:04:09 -0600

    debug

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -142,7 +142,7 @@ resource "aws_security_group" "vpc_endpoints" {
 
 resource "aws_vpc_endpoint" "secretsmanager" {
   vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.secretsmanager"
+  service_name        = "com.amazonaws.${data.aws_region.current.id}.secretsmanager"
   vpc_endpoint_type   = "Interface"
   subnet_ids          = aws_subnet.private[*].id
   security_group_ids  = [aws_security_group.vpc_endpoints.id]
